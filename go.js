63ca60cdf7bf0cad84aa35ebe513a211bfd866fc
Subham1901 <subhamprasad1901@gmail.com>
2025-11-21 17:06:26 +0530

    [ABC-182] Squash commit example

--- go.js
+++ go.js
@@ -1,3 +1,15 @@
 console.log("go");
 console.log("most");
 console.log("whhe");
+
+console.log("go");
+console.log("most");
+console.log("whhe");
+
+console.log("go");
+console.log("most");
+console.log("whhe");
+
+console.log("go");
+console.log("most");
+console.log("whhe");
