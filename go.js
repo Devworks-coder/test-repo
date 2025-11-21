9dbdfd5e9c453373f2a40d377932ef183e51cf70
Subham1901 <subhamprasad1901@gmail.com>
2025-11-21 16:57:23 +0530

    second commit

--- go.js
+++ go.js
@@ -5,3 +5,11 @@ console.log("whhe");
 console.log("go");
 console.log("most");
 console.log("whhe");
+
+console.log("go");
+console.log("most");
+console.log("whhe");
+
+console.log("go");
+console.log("most");
+console.log("whhe");
